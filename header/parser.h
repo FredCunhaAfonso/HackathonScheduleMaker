d3882f28539b02c0411c95cf533aad3bfd79675e
Fred <137807927+FredCunhaAfonso@users.noreply.github.com>
2024-04-06 19:31:12 +0100

    Fred/parser (#1)
    * print parser
    
    * print parser
    
    * add git ignore, skibidi mogged mewing Brian Griffin

--- header/parser.h
+++ header/parser.h
@@ -1,11 +1,10 @@
+#include <stdio.h>
+
 #ifndef PARSER_H
 #define PARSER_H
 
-#include <stdio.h>
-
-FILE * get_user_files(const char *);
-FILE * create_user_files(const char *);
-
-
+FILE * get_user_files(char *);
+void set_user_file(char * username, int * quantidade_de_tempo_disponivel, char ** disciplinas, char ** date, double * horas, int quantidade_disciplinas);
+void print_user_file(char * username);
 
 #endif
